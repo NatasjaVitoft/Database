861786aa059211528127da7cd5cd33588a9c3482
NatasjaVitoft <cph-nn194@cphbusiness.dk>
2025-05-13 09:00:26 +0200

    setup

---
+++ backend/target/debug/backend.d
@@ -0,0 +1,1 @@
+C:\Users\natas\Desktop\Database_exam\backend\target\debug\backend.exe: C:\Users\natas\Desktop\Database_exam\backend\src\main.rs
---
+++ backend/target/debug/deps/backend.d
@@ -0,0 +1,5 @@
+C:\Users\natas\Desktop\Database_exam\backend\target\debug\deps\backend.exe: src\main.rs
+
+C:\Users\natas\Desktop\Database_exam\backend\target\debug\deps\backend.d: src\main.rs
+
+src\main.rs:
