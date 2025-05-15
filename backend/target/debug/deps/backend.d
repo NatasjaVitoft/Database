19bac53fdedcf05ce1ed76e04eb57464a504ebd4
kotteletfisk <ostekugle@gmail.com>
2025-05-15 10:30:03 +0200

    cargo clean

--- backend/target/debug/deps/backend.d
+++
@@ -1,5 +0,0 @@
-C:\Users\natas\Desktop\Database_exam\backend\target\debug\deps\backend.exe: src\main.rs
-
-C:\Users\natas\Desktop\Database_exam\backend\target\debug\deps\backend.d: src\main.rs
-
-src\main.rs:
